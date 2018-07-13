453161861ba87b55ba28ec8a3a9d79527f1b58c0
AdiKaran <karan.aditya@gmail.com>
2018-07-13 17:34:49 -0400

    An efficient solution using the Fundamental Theorem of Arithmetic

--- Problem3.py
+++ Problem3.py
@@ -1,21 +1,38 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
-def SeiveEratosthenes(n):
+# Fundamental Theorem of Arithmetic:
+    # Every integer i>=2 can be expressed as a rpoduct of prime numbers
 
-    prime_factors = [True for i in range(n)]
-    prime_factors[0] = prime_factors[1] = False 
 
-    for i in range( len(prime_factors)):
 
-        if prime_factors[i] == True :
-            if n % i == 0:
-                yield i
-            for x in range(i*i, n, i):
-                prime_factors[x] = False
-             
 
-def PrimeFactor(n):
-    print( [x for x in SeiveEratosthenes(n)] )
-   
-PrimeFactors(600851475143)
+def PrimeFactors(num):
+    p = 2
+    FactList = []
+    while p * p <= num:
+        
+        if num % p == 0 :
+            
+            num = num // p
+          
+            FactList.append(p)
+
+        elif p == 2:
+            p =3
+        
+        else :
+            p += 2
+    if num > p : 
+        FactList.append(num)
+
+    return FactList
+
+def List_Prime_Factors(integer):
+
+    print([ x for x in PrimeFactors(integer)])
+
+
+List_Prime_Factors(600851475143)
+
+
